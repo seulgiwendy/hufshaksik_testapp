a16287a8f19b57a034bdf745ddd0801a3a35d090
wheejuni <wheejuni@gmail.com>
2017-08-02 10:31:32 +0900

    -포맷팅 준

--- src/main/java/com/wheejuni/spring/GetReqController.java
+++ src/main/java/com/wheejuni/spring/GetReqController.java
@@ -6,26 +6,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
+
 @RestController
 public class GetReqController {
-	@RequestMapping (value = "/keyboard", method = RequestMethod.GET)
+	@RequestMapping(value = "/keyboard", method = RequestMethod.GET)
 	public String getKeyboard() {
-		
+
 		JSONObject keyboardJSON = new JSONObject();
 		keyboardJSON = KeyboardReturn.returnKeySet();
 		return keyboardJSON.toString();
-		
-		
+
 	}
-	@RequestMapping (value = "/message", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/message", method = RequestMethod.POST)
 	public String postMessage(@RequestBody String content) throws Exception {
 		JSONParser jp = new JSONParser();
 		JSONObject msgJSON = new JSONObject();
 		JSONObject reqJSON = new JSONObject();
 		Object parse = jp.parse(content);
-		reqJSON = (JSONObject)parse;
+		reqJSON = (JSONObject) parse;
 		msgJSON = MsgHandler.readMessage(reqJSON.get("content").toString());
 		return msgJSON.toString();
 	}
-	
+
 }
--- src/main/java/com/wheejuni/spring/KeyboardReturn.java
+++ src/main/java/com/wheejuni/spring/KeyboardReturn.java
@@ -1,21 +1,21 @@
 package com.wheejuni.spring;
+
 import org.json.simple.*;
 import org.json.simple.JSONObject;
 import java.util.ArrayList;
 
-
 public class KeyboardReturn {
-	
+
 	static JSONObject returnKeySet() {
 		JSONObject returnKey = new JSONObject();
-		ArrayList <String> keyList = new ArrayList<String>();
+		ArrayList<String> keyList = new ArrayList<String>();
 		keyList.add("인문관식당");
 		keyList.add("교수회관식당");
 		keyList.add("업데이트 일정 안내");
-		
+
 		returnKey.put("type", "buttons");
 		returnKey.put("buttons", keyList);
-		
+
 		return returnKey;
 	}
 
